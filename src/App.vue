3e1036fba9d56f27eddbb2225e4c668d0c0ebc13
YongxinWangGH <umay.yx.wang@gmail.com>
2018-12-11 15:58:23 +1100

    tabbar component of footer

--- src/App.vue
+++ src/App.vue
@@ -3,13 +3,19 @@
     <div class="g-view-container">
     	<router-view/>
     </div>
-    <div class="g-footer-container"></div>
+    <div class="g-footer-container">
+    	<c-tabbar/>
+    </div>
   </div>
 </template>
 
 <script>
+	import CTabbar from 'components/tabbar'
 export default {
-  name: 'App'
+  name: 'App',
+  components: {
+  	CTabbar
+  }
 }
 </script>
 
---
+++ src/components/tabbar/index.vue
@@ -0,0 +1,51 @@
+<template>
+	<div class="g-tabbar">
+		<router-link class="g-tabbar-item" to="/home">
+			<i class="iconfont icon-home"></i>
+			<span>Home</span>
+		</router-link>
+		<router-link class="g-tabbar-item" to="/category">
+			<i class="iconfont icon-category"></i>
+			<span>Category</span>
+		</router-link>
+		<router-link class="g-tabbar-item" to="/cart">
+			<i class="iconfont icon-cart"></i>
+			<span>Shopping Cart</span>
+		</router-link>
+		<router-link class="g-tabbar-item" to="/personal">
+			<i class="iconfont icon-personal"></i>
+			<span>Profile</span>
+		</router-link>
+	</div>
+</template>
+
+<script>
+	export default {
+		name: 'CTabbar'
+	}
+</script>
+
+<style lang="scss" scoped>
+@import "~assets/scss/mixins";
+
+	.g-tabbar{
+		display: flex;
+		width: 100%;
+		height: $tabbar-height;
+		background-color: #fff;
+
+		&-item {
+			flex: 1;
+			@include flex-center(column);
+		}
+
+		.iconfont {
+			margin-bottom: 4px;
+			font-size: $icon-font-size;
+		}
+
+		.router-link-active {
+			color: $link-active-color;
+		}
+	}
+</style>
