d1ae5981e9f3072035984c6b4f46a12f7a7d20dd
YongxinWangGH <umay.yx.wang@gmail.com>
2018-12-12 13:27:06 +1100

    swiper-local data

---
+++ src/base/slider/index.vue
@@ -0,0 +1,74 @@
+<template>
+	<swiper :options="swiperOption">
+		<slot></slot>
+		<div class="swiper-pagination" v-if="pagination" slot="pagination"></div>
+	</swiper>
+	
+
+</template>
+
+<script>
+	import {swiper} from 'vue-awesome-swiper'
+
+	export default {
+		name: 'MeSlider',
+		components: {
+			swiper
+		},
+		props: {
+			direction: {
+				type: String,
+				default: 'horizontal',
+				validator(val){
+					return ['horizontal', 'vertical'].indexOf(val) > -1;
+				}
+			},
+
+			interval: {
+				type: Number,
+				default: 3000,
+				validator(val) {
+					return val >= 0;
+				}
+			},
+
+			loop: {
+				type: Boolean,
+				default: true
+			},
+
+			pagination: {
+				type: Boolean,
+				default: true
+			}
+		},
+		data(){
+			return {
+				swiperOption: {
+		          watchOverflow: true,
+		          direction: this.direction,
+		          autoplay: this.interval ? {
+		            delay: this.interval,
+		            disableOnInteraction: false
+		          } : false,
+		          slidesPerView: 1,
+		          loop: this.loop,
+		          pagination: {
+		            el: this.pagination ? '.swiper-pagination' : null
+		          }
+		      }
+			}
+		}
+	}
+</script>
+
+<style lang="scss" scoped>
+@import "~assets/scss/mixins";
+	
+	.swiper-container {
+		width: 100%;
+		height: 100%;
+	}
+	
+	
+</style>
--- src/pages/home/index.vue
+++ src/pages/home/index.vue
@@ -3,7 +3,9 @@
 		<header class="g-header-container">
 			<home-header></home-header>
 		</header>
-		<div></div>
+		<div>
+			<home-slider></home-slider>
+		</div>
 		<div class="g-backtop-container"></div>
 		<router-view></router-view>
 	</div>
@@ -11,10 +13,12 @@
 
 <script>
 	import HomeHeader from './header'
+	import HomeSlider from './slider'
 	export default {
 		name: 'Home',
 		components: {
-			HomeHeader
+			HomeHeader,
+			HomeSlider
 		}
 	}
 </script>
