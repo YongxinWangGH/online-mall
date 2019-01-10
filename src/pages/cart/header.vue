59d47b236956c4a7a2735fdb7cafefc601870be6
YongxinWangGH <umay.yx.wang@gmail.com>
2019-01-10 20:24:59 +1100

    cart-page

---
+++ src/pages/cart/header.vue
@@ -0,0 +1,25 @@
+<template>
+	<me-navbar>
+		<div class="text" slot="center">Shopping Cart</div>
+		<i class="iconfont icon-msg" slot="right"></i>
+	</me-navbar>
+</template>
+
+<script>
+	import MeNavbar from 'base/navbar'
+	export default {
+		name: 'cartHeader',
+		components: {
+			MeNavbar
+		}
+	}
+</script>
+
+<style lang="scss" scoped>
+@import "~assets/scss/mixins";
+	.text{
+		text-align: center;
+		font-size: $font-size-l;
+	}
+	
+</style>
--- src/pages/cart/index.vue
+++ src/pages/cart/index.vue
@@ -1,17 +1,40 @@
 <template>
-	<div class="">
-		Cart
+	<div class="cart">
+		<header class="g-header-container">
+			<cart-header/>
+		</header>
+		<me-scroll scrollbar>
+			<div class="imgwrapper">
+				<img src="./cart1png.png" alt="">
+			</div>
+		</me-scroll>
 	</div>
 </template>
 
 <script>
+	import cartHeader from './header'
+	import MeScroll from 'base/scroll'
 	export default {
-		name: 'Cart'
+		name: 'Cart',
+		components: {
+			cartHeader,
+			MeScroll
+		}
 	}
 </script>
 
 <style lang="scss" scoped>
 @import "~assets/scss/mixins";
+
+	.imgwrapper{
+		width:100%;
+		height:100%;
+		padding-top: $navbar-height;
+		img{
+			width:100%;
+			height:100%;
+			}
+	}
 
 	
 </style>
